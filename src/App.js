492c68a0d362933662134134ab77123d7c207835
Abinash Mohapatra <abinash@expensebit.com>
2020-06-28 14:02:37 +0530

    set the propTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { useSelector, useDispatch } from 'react-redux';
-import { Switch, Route } from 'react-router-dom';
+import { useSelector, useDispatch, connect } from 'react-redux';
+import { Switch, Route, withRouter } from 'react-router-dom';
 
 import lightTheme from './theme/Light';
 import darkTheme from './theme/Dark';
@@ -17,12 +17,11 @@ import Default from './components/Default/Default';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
-
-const App = () => {
+const App = (props) => {
+    console.log(props)
     const dispatch = useDispatch();
     const config = useSelector(state => state.config);
-    const products = useSelector(state => state.productReducer.products);
-    // console.log(product)
+    // const products = useSelector(state => state.productReducer.products);
     useEffect(() => {
         if(!config.darkMode) {
             dispatch(darkModeAction(window.localStorage.getItem('theme')))
@@ -40,7 +39,7 @@ const App = () => {
                 <React.Fragment>
                     <Navbar themeChange={themeChange} />
                     <Switch>
-                        <Route exact path="/" component={() => <ProductList products={products} />} />
+                        <Route exact path="/" component={() => <ProductList products={props.products} />} />
                         <Route path="/details" component={ProductDetails} />
                         <Route path="/cart" component={Cart} />
                         <Route component={Default} />
@@ -51,4 +50,11 @@ const App = () => {
     );
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        products: state.productReducer.products,
+        detailProducts: state.productReducer.detailedProduct
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(App));
